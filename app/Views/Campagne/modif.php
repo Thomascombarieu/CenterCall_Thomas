413a2cba06b2a1419cf11178e5934fea790e8e55
Thomascombarieu <128461011+Thomascombarieu@users.noreply.github.com>
2025-05-28 11:08:55 +0200

    Update modif.php

--- app/Views/Campagne/modif.php
+++ app/Views/Campagne/modif.php
@@ -23,7 +23,7 @@
 
     <label class="required" for="EVENEMENT">Evènements</label>
     
-    <input type="text" id="EVENEMENT" name="EVENEMENT"required value="<?= $campagne['TITRE'] ?>"placeholder="Saisissez l'évènement'" required minlength="3"
+    <input type="text" id="EVENEMENT" name="EVENEMENT"required value="<?= $campagne['EVENEMENT'] ?>"minlength="3"
         maxlength="40" size="50"required><?= $campagne['EVENEMENT'] ?>
 
 
