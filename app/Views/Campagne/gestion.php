79290f5ddb0e5cf6d17f85c6d25ce496aa6b0546
Thomascombarieu <128461011+Thomascombarieu@users.noreply.github.com>
2025-05-28 10:48:30 +0200

    Update gestion.php

--- app/Views/Campagne/gestion.php
+++ app/Views/Campagne/gestion.php
@@ -24,7 +24,7 @@ foreach ($listeCampagnes as $campagne) {
 
     $table->addRow(
         $campagne['TITRE'],
-         $campagne['TITRE'],
+        '<a></a>',
         '<a><button class="button button-voir-campagne">Voir</button></a>',
         '<a href="' . url_to('modif_campagne_get', $campagne['ID_CAMPAGNE']) . '"><button class="button button-modifier">Modifier</button></a>'
     );
